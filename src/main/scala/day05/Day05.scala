ea72c4191012794ceaf584da0a9947162d8bdec7
Jannyboy11 <Jannyboy11@gmail.com>
2023-12-04 22:59:56 +0100

    prepare for day 5

---
+++ src/main/scala/day05/Day05.scala
@@ -0,0 +1,17 @@
+package day05
+
+import scala.io.Source
+
+val source = Source.fromResource("day05.in")
+
+
+
+@main def main(): Unit = {
+
+    val result1 = ???
+    println(result1)
+
+    val result2 = ???
+    println(result2)
+
+}
