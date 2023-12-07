d7baa3fce0dbee620e1b05e92b2edced24a6d224
Jannyboy11 <Jannyboy11@gmail.com>
2023-12-07 19:31:14 +0100

    day 7 part 1.

--- src/main/scala/day07/Day07.scala
+++ src/main/scala/day07/Day07.scala
@@ -4,6 +4,7 @@ import day07.WinCondition.HighCard
 
 import scala.annotation.switch
 import scala.collection.SeqView.Sorted
+import scala.collection.mutable
 import scala.deriving.Mirror
 import scala.io.Source
 
@@ -44,56 +45,69 @@ enum Rank(val char: Char):
     case King extends Rank('K')
     case Ace extends Rank('A')
 
-enum WinCondition:
+enum WinCondition extends java.lang.Enum[WinCondition]:
+    //extend java.lang.Enum, so that this class implements Comparable
+    //when then in turn makes the Ordering[WinCondition] instance implicitly available :-)
     case HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind
-
-    object OnePair:
-        def is(sortedHand: Hand): Boolean = sortedHand match
-            case Seq(a, b, c, d, e) if a == b || b == c || c == d || d == e => true
-            case _ => false
-    object TwoPair:
-        def is(sortedHand: Hand): Boolean = sortedHand match
-            case Seq(a, b, c, d, e) if (a == b && (c == d || d == e)) || (a == c && d == e) => true
-            case _ => false
-    object ThreeOfAKind:
-        def is(sortedHand: Hand): Boolean =
-            allEqual(sortedHand.head, sortedHand.take(3)) || allEqual(sortedHand.last, sortedHand.drop(2))
-    object FullHouse:
-        def is(sortedHand: Hand): Boolean =
-            (allEqual(sortedHand.head, sortedHand.take(3)) && allEqual(sortedHand.last, sortedHand.drop(3))) ||
-                (allEqual(sortedHand.head, sortedHand.take(2)) && allEqual(sortedHand.last, sortedHand.drop(2)))
-    object FourOfAKind:
-        def is(sortedHand: Hand): Boolean =
-            allEqual(sortedHand.head, sortedHand.take(4)) || allEqual(sortedHand.last, sortedHand.drop(1))
-    object FiveOfAKind:
-        def is(sortedHand: Hand): Boolean = allEqual(sortedHand.head, sortedHand)
-
-def sortHand(hand: Hand): Hand = hand.sortBy(_.ordinal)(using summon[Ordering[Int]].reverse)
 
 def allEqual[A](to: A, coll: Iterable[A]): Boolean = coll.forall(_ == to)
 
+object OnePair:
+    def is(sortedHand: Hand): Boolean = sortedHand match
+        case Seq(a, b, c, d, e) if a == b || b == c || c == d || d == e => true
+        case _ => false
+object TwoPair:
+    def is(sortedHand: Hand): Boolean =
+        sortedHand match
+            case Seq(a, b, c, d, e) if (a == b && (c == d || d == e)) || (a == c && d == e) || (b == c && d == e) =>  true
+            case _ => false
+object ThreeOfAKind:
+    def is(sortedHand: Hand): Boolean =
+        allEqual(sortedHand(0), sortedHand.take(3)) || allEqual(sortedHand(1), sortedHand.slice(1, 4)) || allEqual(sortedHand(2), sortedHand.slice(2, 5))
+object FullHouse:
+    def is(sortedHand: Hand): Boolean =
+        (allEqual(sortedHand.head, sortedHand.take(3)) && allEqual(sortedHand.last, sortedHand.drop(3))) ||
+            (allEqual(sortedHand.head, sortedHand.take(2)) && allEqual(sortedHand.last, sortedHand.drop(2)))
+object FourOfAKind:
+    def is(sortedHand: Hand): Boolean =
+        allEqual(sortedHand.head, sortedHand.take(4)) || allEqual(sortedHand.last, sortedHand.drop(1))
+object FiveOfAKind:
+    def is(sortedHand: Hand): Boolean = allEqual(sortedHand.head, sortedHand)
+
+def sortHand(hand: Hand): Hand = hand.sortBy(_.ordinal)(summon[Ordering[Int]].reverse)
+
 def winCondition(sortedHand: Hand): WinCondition = {
-    import WinCondition.*
-    //TODO can we use typeclass derivation instead?
     if FiveOfAKind.is(sortedHand) then
-        FiveOfAKind
+        WinCondition.FiveOfAKind
     else if FourOfAKind.is(sortedHand) then
-        FiveOfAKind
+        WinCondition.FourOfAKind
     else if FullHouse.is(sortedHand) then
-        FullHouse
+        WinCondition.FullHouse
     else if ThreeOfAKind.is(sortedHand) then
-        ThreeOfAKind
+        WinCondition.ThreeOfAKind
     else if TwoPair.is(sortedHand) then
-        TwoPair
+        WinCondition.TwoPair
     else if OnePair.is(sortedHand) then
-        OnePair
+        WinCondition.OnePair
     else
-        HighCard
+        WinCondition.HighCard
 }
+
+//TODO do not sort more than once.
+val handOrdering: Ordering[Hand] = Ordering.by[Hand, WinCondition](hand => winCondition(sortHand(hand)))
+    .orElseBy(hand => hand.map(rank => rank.ordinal))(using Ordering.Implicits.seqOrdering)
+
+def makeRankings(cards: Seq[Hand]): Map[Hand, Int] =
+    cards.sorted(using handOrdering).zip(LazyList.iterate(1)(_ + 1)).toMap
 
 @main def main(): Unit = {
 
-    val result1 = ???
+    val result1 = {
+        val hands = input.map((hand, bid) => hand)
+        val rankings = makeRankings(hands)
+        val strengths = input.map((hand, bid) => (rankings(hand) * bid).toLong)
+        strengths.sum
+    }
     println(result1)
 
     val result2 = ???
